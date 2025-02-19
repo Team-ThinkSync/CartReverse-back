38512e0ec693bae490faa7d0e94a3754769b7306
leejinuk <leejinouk123@kakao.com>
2025-02-19 14:09:19 +0900

    feature: 리뷰 Controller(API) 구현 (#34)

---
+++ src/main/java/com/project/webshopproject/review/ReviewRestController.java
@@ -0,0 +1,62 @@
+package com.project.webshopproject.review;
+
+import com.project.webshopproject.common.RestApiResponseDto;
+import com.project.webshopproject.review.dto.ReviewRequestDto;
+import com.project.webshopproject.review.dto.ReviewResponseDto;
+import com.project.webshopproject.security.UserDetailsImpl;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequiredArgsConstructor
+public class ReviewRestController {
+    private final ReviewService reviewService;
+
+    @PostMapping("/products/{productId}/reviews")
+    public ResponseEntity<RestApiResponseDto<String>> createReview(
+            @PathVariable final Long productId,
+            @RequestPart("dto") final ReviewRequestDto requestDto,
+            @RequestPart("images") final List<MultipartFile> images,
+            @AuthenticationPrincipal final UserDetailsImpl userDetails
+    ) {
+        reviewService.createReview(productId, requestDto, images, userDetails.getUser());
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(RestApiResponseDto.of("리뷰가 등록되었습니다."));
+    }
+
+    @GetMapping("/products/{productId}/reviews")
+    public ResponseEntity<RestApiResponseDto<List<ReviewResponseDto>>> getAllReviews(
+            @PathVariable Long productId
+    ) {
+        List<ReviewResponseDto> responseDto = reviewService.getAllReviews(productId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(RestApiResponseDto.of("리뷰를 전체조회하였습니다.", responseDto));
+    }
+
+    @PatchMapping("/products/{productId}/reviews/{reviewId}")
+    public ResponseEntity<RestApiResponseDto<String>> updateReview(
+            @PathVariable Long reviewId,
+            @RequestPart("dto") ReviewRequestDto requestDto) {
+        reviewService.updateReview(reviewId, requestDto);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(RestApiResponseDto.of("리뷰가 수정되었습니다."));
+    }
+
+    @DeleteMapping("/products/{productId}/reviews/{reviewId}")
+    public ResponseEntity<RestApiResponseDto<String>> deleteReview(@PathVariable Long reviewId) {
+        reviewService.deleteReview(reviewId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(RestApiResponseDto.of("리뷰가 삭제되었습니다."));
+    }
+}
