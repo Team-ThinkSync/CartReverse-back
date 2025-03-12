bff42229744d378120d7c580cb9ed9a063b9a28d
leejinuk <leejinouk123@kakao.com>
2025-03-12 20:30:59 +0900

    refactor: rate에 validation 추가 (#36)

--- src/main/java/com/project/webshopproject/review/dto/ReviewRequestDto.java
+++ src/main/java/com/project/webshopproject/review/dto/ReviewRequestDto.java
@@ -1,7 +1,10 @@
 package com.project.webshopproject.review.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+
 public record ReviewRequestDto(
         String title,
         String content,
-        int rate
+        @Min(0) @Max(5) Integer rate // 0 ~ 5 사이의 값만 허용
 ) {}
