0ea70ee359ba5936c7696625bc7ad3396ceeac77
leejinuk <leejinouk123@kakao.com>
2025-03-12 20:30:16 +0900

    feature: Review Entity에 수정 메서드 추가 (#35)

--- src/main/java/com/project/webshopproject/review/entity/Review.java
+++ src/main/java/com/project/webshopproject/review/entity/Review.java
@@ -3,6 +3,7 @@ package com.project.webshopproject.review.entity;
 import com.project.webshopproject.common.TimeStamp;
 import com.project.webshopproject.product.entity.Product;
 import com.project.webshopproject.user.entity.User;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -11,8 +12,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -38,6 +42,9 @@ public class Review extends TimeStamp {
     @JoinColumn(name = "product_id", nullable = false)
     private Product product;
 
+    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<ReviewImage> reviewImages = new ArrayList<>();
+
     @Column(nullable = false, length = 50)
     private String title;
 
@@ -58,5 +65,17 @@ public class Review extends TimeStamp {
         this.content = content;
         this.rate = rate;
         this.updatedAt = updatedAt;
+    }
+
+    public void updateTitle(String title) {
+        this.title = title;
+    }
+
+    public void updateContent(String content) {
+        this.content = content;
+    }
+
+    public void updateRate(int rate) {
+        this.rate = rate;
     }
 }
