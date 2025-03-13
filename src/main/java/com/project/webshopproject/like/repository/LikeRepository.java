343343880f867ce8f39d3242bd2b4c7d1579f33e
wooseokJo <dntjrwh@gmail.com>
2025-03-13 19:00:06 +0900

    refactor: 카테고리 삭제시 상품,이미지,리뷰,좋아요 삭제 #30

--- src/main/java/com/project/webshopproject/like/repository/LikeRepository.java
+++ src/main/java/com/project/webshopproject/like/repository/LikeRepository.java
@@ -15,4 +15,6 @@ public interface LikeRepository extends JpaRepository<Likes, Long> {
     // 특정 사용자의 좋아요 찾기
     Optional<Likes> findByUser_UserIdAndProduct_ProductId(Long userId, Long productId);
     Optional<Likes> findByUser_UserIdAndReview_ReviewId(Long userId, Long reviewId);
+
+    void deleteByProduct_ProductId(Long productId);
 }
--- src/main/java/com/project/webshopproject/review/ReviewRepository.java
+++ src/main/java/com/project/webshopproject/review/ReviewRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findByProduct_ProductId(Long productId);
+    void deleteByProduct_ProductId(Long productId);
 }
